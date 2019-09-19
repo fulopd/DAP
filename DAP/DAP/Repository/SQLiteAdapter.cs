ef3a51bb0207e17acc91b0103bd32df49424e109
fulopd <fulopdaniel@gmail.com>
2019-09-19 10:10:31 +0200

    Telepítve NuGet: System.Data.SQLite.Core Repositoryhoz SQLite adapter hozzáadva

---
+++ DAP/DAP/Repository/SQLiteAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Telepítve NuGet: System.Data.SQLite.Core
+namespace DAP.Repository
+{
+    class SQLiteAdapter
+    {
+    }
+}
